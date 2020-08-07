80ad9422701a390cd3df94ac6def1cd78d190763
Erik <kimseyerik@gmail.com>
2020-08-07 05:48:20 -0400

    Initial with a ton of stuff

---
+++ Assets/Scripts/FacePlaneController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FacePlaneController : MonoBehaviour
+{
+    GameObject[] planes;
+    public float degToRotate;
+    
+    void Start()
+    {
+        CollectAllPlanes();
+    }
+
+    private void CollectAllPlanes(){
+        planes = GameObject.FindGameObjectsWithTag("face_plane");
+        Debug.Log(planes[0]);
+    }
+
+    public void RotatePlanes(){
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/TouchController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TouchController : MonoBehaviour
+{
+    // Start is called before the first frame update
+    private Vector3 touchPosition;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.touchCount > 0 || Input.GetMouseButton(0)){
+            // Touch touch = Input.GetTouch(0);
+            touchPosition = Input.mousePosition;
+            Debug.Log(touchPosition);
+        }
+    }
+}
